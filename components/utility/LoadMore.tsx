8d1e6bb7d76f89ce0cb8028e8ca1b8863252b5b7
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-09 22:29:58 -0500

    feat: add LoadMore component

---
+++ components/utility/LoadMore.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { fetchAnimes } from "@/services/animes";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { AnimeProp } from "@/interfaces/interface";
+import AnimeList from '../animes/AnimeList';
+import Image from "next/image";
+import { fetchAnime } from "@/services/action";
+
+let page: number = 2;
+
+export type AnimeCard = JSX.Element;
+
+function LoadMore () {
+  const { ref, inView } = useInView();
+  // const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   if (inView) {
+  //     fetchAnimes({ page: page }).then((res) => {
+  //       setData([...data, ...res]);
+  //       page++;
+  //     });
+  //   }
+  // }, [inView, data]);
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      // Add a delay of 500 milliseconds
+      const delay = 500;
+
+      const timeoutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      // Clear the timeout if the component is unmounted or inView becomes false
+      return () => clearTimeout(timeoutId);
+    }
+  }, [inView, data, isLoading]);
+
+
+  return (
+    <>
+      {/* <AnimeList animes={data} /> */}
+      <section
+        className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10"
+      >
+        {data}
+      </section>
+
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
