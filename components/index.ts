c4ff6c07dd668dce5d29d4d1a8b31eafb3fa27cb
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-09 22:22:10 -0500

    feat: add barrel file to components folder

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+export { default as AnimeCard } from "./animes/AnimeCard";
+export { default as AnimeList } from "./animes/AnimeList";
+
+export { default as Footer } from "./common/Footer";
+export { default as Hero } from "./common/Hero";
+
+export { MotionDiv } from "./motion/MotionDiv";
+
+export { default as LoadMore } from "./utility/LoadMore";
