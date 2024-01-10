55a531f901f7e01a16e6906c8cc2a241a84ca671
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-09 22:29:21 -0500

    chore: update fetchAnime to return JSX elements

---
+++ services/action.tsx
@@ -0,0 +1,19 @@
+"use server";
+
+import { AnimeCard } from "@/components";
+import { AnimeProp } from "@/interfaces/interface";
+
+
+const MAX_LIMIT = 8;
+
+export async function fetchAnime (page: number) {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+  );
+
+  const data = await response.json();
+
+  return data.map((anime: AnimeProp, index: number) => (
+    <AnimeCard key={anime.id} anime={anime} index={index} />
+  ));
+}
