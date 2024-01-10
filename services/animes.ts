d8d276b665d57e78b467ee2047b0d0fa89e286ee
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-09 22:26:06 -0500

    feat: implement server-side fetching with fetchAnimes function

---
+++ services/animes.ts
@@ -0,0 +1,33 @@
+"use server";
+
+const API_BASE_URL = "https://shikimori.one/api/animes";
+
+const MAX_LIMIT = 8;
+const ORDER_TYPE = "popularity";
+
+interface AnimesOptions {
+  page?: number;
+  limit?: number;
+  order?: string;
+}
+
+export const fetchAnimes = async ({
+  page = 1,
+  limit = MAX_LIMIT,
+  order = ORDER_TYPE,
+}: AnimesOptions) => {
+  try {
+    const res = await fetch(
+      `${API_BASE_URL}?page=${page}&limit=${limit}&order=${order}`
+    );
+
+    if (!res.ok)
+      throw new Error(`Failed to fetch animes. Status: ${res.status}`);
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`Error getting animes`);
+  }
+};
